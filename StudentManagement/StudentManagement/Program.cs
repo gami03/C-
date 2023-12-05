ae68283244312fe14d505db48ce87f8ad502085f
ybg <tjdrhd798@naver.com>
2023-12-05 22:30:57 +0900

    chore: 학생 클래스 만들기

--- StudentManagement/StudentManagement/Program.cs
+++ StudentManagement/StudentManagement/Program.cs
@@ -1,7 +1,16 @@
 ﻿internal class Program
 {
-	private static void Main(string[] args)
+	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		Student.WhoAreYou();
 	}
+
+	static class Student
+	{
+		static public void WhoAreYou()
+		{
+            Console.WriteLine("나는 학생 클래스 입니다.");
+        }
+	}
+
 }
